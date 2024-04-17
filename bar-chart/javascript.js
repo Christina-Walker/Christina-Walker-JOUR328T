3f8c4e88fb11d6a426603f7bc6097c4d0bcfa780
Christina-Walker <cwalke12@terpmail.umd.edu>
2024-04-17 10:03:35 -0400

    index.html

--- bar-chart/javascript.js
+++ bar-chart/javascript.js
@@ -2,21 +2,14 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['2015', '2016', '2017', '2018', '2019', '2020'],
+        labels: ['America', 'East Asia & Pacific', 'Europe & Central Asia', 'Middle East & North Africa', 'South Asia', 'Sub-Saharan Africa'],
         datasets: [{
-            label: '# of Apples',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Average of GDP per Capita',
+            data: [18791.13, 14480.30, 20818.45, 13856.00, 2505.17, 2569.00],
             backgroundColor: [
-                '#ff0800'
+                '#6495ED'
             ]
         },
-        {
-            label: '# of Oranges',
-            data: [22, 10, 23, 6, 22, 13],
-            backgroundColor: [
-                '#FFA500'
-            ],
-        }
     ]
     },
     options: {
@@ -24,19 +17,23 @@ var myChart = new Chart(ctx, {
         plugins: {
             title: {
                 display: true,
-                text: 'Apples and Oranges by year',
+                text: 'Average GDP per Capita by Regions of the World',
                 font: {
                     size: 18
                 }
             },
             subtitle: {
                 display: true,
-                text: 'For all but one year, oranges outnumbered apples'
+                text: 'Europe and central Asia had the highest recorded average GDP per capita at $20,818.45. South Asia and sub-Saharan Africa had the lowest GDP per capita, both barely surpassing $2,500.'
             }
         },
         scales: {
             y: {
-                beginAtZero: true
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Average GDP per Capita ($)'
+                  }
             }
         },
     },
